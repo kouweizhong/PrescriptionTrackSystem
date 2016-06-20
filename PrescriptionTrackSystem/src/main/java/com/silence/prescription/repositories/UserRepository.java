f45c9bae8f86b156da2c58455261d5d250556e3e
silence940109 <695412269@qq.com>
2016-06-20 18:56:04 +0800

    用户数据库的操作，增加了对highchart数据库的处理

--- PrescriptionTrackSystem/src/main/java/com/silence/prescription/repositories/UserRepository.java
+++ PrescriptionTrackSystem/src/main/java/com/silence/prescription/repositories/UserRepository.java
@@ -22,7 +22,20 @@ public interface UserRepository extends JpaRepository<User, Integer>{
 	public void update(@Param("name") String name,@Param("birthday") Date birthday,
 			@Param("telephone") String telephone,@Param("insuranceCompany") String insuranceCompany,
 			@Param("policyNumber") String policyNumber,@Param("id") Integer id);
-	
+
+	//根据搜索关键字查询用户
 	@Query("from User u where concat(u.name,u.birthday,u.telephone,u.insuranceCompany,u.policyNumber) like CONCAT('%',:search,'%')")
 	public List<User> findUserByAll(@Param("search") String search);
+	
+	//根据承保公司计算用户的数量
+	@Query("select count(*),insuranceCompany from User u group by u.insuranceCompany")
+	public List<Object[]> countUser();
+
+	//根据不同的年龄段统计用户的数量
+	@Query(value="select count(*),left(birthday,4) from user group by left(birthday,4)",nativeQuery=true)
+	public List<Object[]> findByAgeCount();
+	
+	//查询用户的出生年份，月份，日期
+	@Query(value="select left(birthday,4),substring(birthday,6,2),substring(birthday,9,2) from user",nativeQuery=true)
+	public List<Object[]> findByYearMonthdayDay();
 }
